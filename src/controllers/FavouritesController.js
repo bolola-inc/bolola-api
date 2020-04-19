94c8af7dbec6e57a23dc23b6770e2cfadc2210ee
nverabgaryan <abgaryannver1993@gmail.com>
2020-04-19 22:21:54 +0400

    Fix authentication

--- src/controllers/FavouritesController.js
+++ src/controllers/FavouritesController.js
@@ -3,14 +3,14 @@ const models = require("../models")
 async function GetFavourites (req, res) {
   const favourites = await models.Favourites.findAll({
     where: {
-      userId: req.user.deviceId
+      userId: req.user.id
     }
   })
   res.send(favourites)
 }
 
 async function CreateFavourites (req, res) {
-  const favourites = await models.Favourites.create({ ...req.body, userId: req.user.deviceId })
+  const favourites = await models.Favourites.create({ ...req.body, userId: req.user.id })
   res.send(favourites)
 }
 
