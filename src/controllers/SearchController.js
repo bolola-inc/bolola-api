07be699662013123d7c5b96ddb5ff2af0c09ae57
TamaraNazaryan <tamara.nazaryan.y@gmail.com>
2020-05-02 14:02:03 +0400

    search endpoint

--- src/controllers/SearchController.js
+++ src/controllers/SearchController.js
@@ -5,16 +5,17 @@
 const models = require('../models');
 
 
-async function GetSearchHistory(req, res){
-    const search = await models.SearchHistory.findAll()
-    where: {
-        userId: req.user.deviceId
-    }
-    console.log(search) 
-}
+async function GetSearchHistory (req, res) {
+    const search = await models.SearchHistory.findAll({
+      where: {
+        userId: req.user.id
+      }
+    })
+    res.send(search)
+  }
 
 async function CreatSearchHistory (req,res) {
-    const search = await models.Search.create(req.body)
+    const search = await models.Search.create({...req.body, userId:req.user.id})
     res.send(search);
 }
 
