fd8d084dadac593c72fedc53792986c08ea74692
GGC_ <ggc.language@gmail.com>
2020-06-03 16:39:39 +0400

    fix db place, station names

--- migrations/20191120184906-station-names.js
+++ migrations/20191120184906-station-names.js
@@ -21,6 +21,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.DATE
       },
+      deletedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      }
     });
   },
   down: (queryInterface, Sequelize) => {
