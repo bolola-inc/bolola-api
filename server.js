c3d73e9af5bad77f2f2f08f2f827afe12c788692
nverabgaryan <abgaryannver1993@gmail.com>
2020-06-06 12:44:10 +0400

    heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const initApp = require("./src/app");
 const env = require("./config/env");
 
 initApp().then((server) => {
-  server.listen(env.APP_PORT || 3000, () => {
+  server.listen(process.env.PORT || env.APP_PORT || 3000, () => {
     console.log(`started ${env.APP_PORT || 3000}`);
   });
 });
